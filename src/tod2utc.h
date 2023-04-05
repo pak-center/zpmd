ebb27e3e020580a7ee027fefc58ec1fd42713205
Przemyslaw Adam Kupisz <125362872+pak-center@users.noreply.github.com>
2023-04-05 12:50:24 +0200

    Add files via upload

--- src/tod2utc.h
+++ src/tod2utc.h
@@ -1,10 +1,15 @@
+#ifndef _TOD2UTC_H
+#define	_TOD2UTC_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
 
 // TIME FUNCTION
-__eu64 tod2utc(__eu64 time)
-{
-    time = (__eu64) time - (__eu64) 0x7d91048bca000000ULL;
-    time = time >> 12;
-    //time = do_div(time, 1000000);
-    time = time / 1000000;
-    return time;
-};
+__eu64 tod2utc(__eu64 time);
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* _TOD2UTC_H */
